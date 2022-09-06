77ce5cb690fcb9abe4afbe1ba185b36704eaae24
beprabin <71381937+beprabin@users.noreply.github.com>
2022-09-06 16:05:12 +0545

    8

--- Q8Currency.java
+++ Q8Currency.java
@@ -13,8 +13,10 @@ import java.awt.event.*;
 public class Q8Currency {
     Q8Currency (){
         Frame f=new Frame("Currency Converter");
-        f.setSize(400,400);
-        f.setLayout(new FlowLayout());
+        Panel p=new Panel();
+        
+        f.setSize(400,300);
+        
         Label l=new Label("Dollar",Label.LEFT);
         TextField t=new TextField(10);
         Label l1=new Label("Nepalese",Label.LEFT);
@@ -22,24 +24,29 @@ public class Q8Currency {
         Label l2=new Label("Euro",Label.LEFT);
         TextField t2=new TextField(10);
         
-        f.add(l);
-        f.add(t);
-        f.add(l1);
-        f.add(t1);
-        f.add(l2);
-        f.add(t2);
+        p.setLayout(new GridLayout(3,2));
+        p.setPreferredSize(new Dimension(200,110));
         
-        l.addKeyListener(new KeyListener(){
+        p.add(l);
+        p.add(t);
+        p.add(l1);
+        p.add(t1);
+        p.add(l2);
+        p.add(t2);
+        f.add(p);
+        f.setLayout(new FlowLayout());
+        t.addKeyListener(new KeyAdapter(){
             int n1;
             
-            public void keyTyped(KeyEvent ae){
-                n1=Integer.parseInt(t.getText());  
-                
-            }
-            public void keyPressed(KeyEvent ae){
-               
-            }
+//            public void keyTyped(KeyEvent a){
+//                n1=Integer.parseInt(t.getText());  
+//              
+//            }
+//            public void keyPressed(KeyEvent b){
+//               
+//            }
             public void keyReleased(KeyEvent ae){
+                n1=Integer.parseInt(t.getText());
                 t1.setText(""+n1*127.62);
                 t2.setText(""+n1*0.89);
             }
